7f5d86d87f10506c842ce54b3dd66bf2a5ef9fee
SijongKim93 <rlatlwhd456@gmail.com>
2024-09-15 16:46:02 +0900

    Setting: 프로젝트 세팅

---
+++ PracticeTCA/PracticeTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTCAApp.swift
+//  PracticeTCA
+//
+//  Created by 김시종 on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
